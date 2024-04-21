77c74169fc63b175d1a16fa810da2146b74bd861
AnasTsya0 <nstsyzm@gmail.com>
2024-04-21 21:26:45 +0700

    Pertemuan 6

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $data['judul']; ?></title>
+    <link rek="stylesheet" href="http://localhost/belajarmvc/public/css/bootstrap.css">
 </head>
 <body>
 
